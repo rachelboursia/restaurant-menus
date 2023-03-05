2fb6565d125978624867b46783e4c2f7e6abbd36
rachelboursia <rachel.l.boursia@gmail.com>
2023-03-05 12:48:48 -0800

    connects sequelize

--- db.js
+++ db.js
@@ -3,6 +3,11 @@ const { Sequelize } = require('sequelize');
 
 // TODO - connect to db via sequelize
 
+const sequelize = new Sequelize({
+    dialect: 'splite',
+    storage: path.join(__dirname, 'db.sqlite')
+});
+
 module.exports = {
     sequelize
 };
